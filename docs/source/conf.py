0c9fb5cee4e090ae71ff5e69a79b87ff746136ec
FPeyrard <fabrice.Peyrard@enseeiht.fr>
2021-03-14 20:44:28 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,9 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ ]
+extensions = [
+	'sphinx.ext.autosectionlabel'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -45,7 +47,7 @@ language = 'fr'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['sphinx.ext.autosectionlabel']
+exclude_patterns = [ ]
 
 
 # -- Options for HTML output -------------------------------------------------
