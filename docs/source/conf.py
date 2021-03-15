53acb8297719633ec18ec6fe5fb998009925e780
FPeyrard <fabrice.Peyrard@enseeiht.fr>
2021-03-15 17:26:17 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -23,6 +24,7 @@ author = 'Fabrice Peyrard'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0'
+
 
 
 # -- General configuration ---------------------------------------------------
